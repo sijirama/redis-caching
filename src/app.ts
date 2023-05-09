af7500344de217cd6f2d0f22da774376aaef4285
sijibomi <gbemilesanmi@gmail.com>
2023-05-09 22:51:58 +0100

    chore: redis set and basic post route

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,19 @@ import Express from "express"
 
 const app = Express()
 const PORT  = process.env.PORT || 5173
-import redis from "redis"
-
-redis.createClient("redis")
+import redis , {RedisClientType} from "redis"
 
 
+const redisUrl = "redis://127.0.0.1:6379"
+const client:RedisClientType = redis.createClient({url:redisUrl})
 
 
+app.use(Express.json())
 
-
+app.post("/" , (req , _res) => {
+    const {key , value} = req.body
+    client.set(key , value)
+})
 
 
 
