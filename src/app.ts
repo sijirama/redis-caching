2ec26a656d68ef4304c39a729a16d1e36b835494
sijibomi <gbemilesanmi@gmail.com>
2023-05-10 00:55:03 +0100

    feat: redis integration

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import Express from "express"
+import axios from "axios"
 
 const app = Express()
 const PORT  = process.env.PORT || 5173
@@ -10,7 +11,7 @@ const client:redis.RedisClientType = redis.createClient({url:redisUrl});
 
 app.use(Express.json())
 
-app.get("/" , (_req , res) => {
+app.get("/ping" , (_req , res) => {
     res.json({message:"PONG"})
 })
 
@@ -27,6 +28,29 @@ app.post("/" , async (req , res) => {
         console.error(error)
         return res.status(500).json({error})
     }
+})
+
+app.get("/" , async (req , res) => {
+    const {key} = req.body
+    try {
+       const response = await client.get(key) 
+       console.log(response)
+        return res.json(response)
+    } catch (error) {
+        console.error(error)
+        return res.status(500).json({error})
+    }
+})
+
+app.get("/posts/:id" , async (req , res)=>{
+    const {id} = req.params
+    const cachedPost = await client.get(`post-${id}`)
+    if (cachedPost){
+        return res.json(JSON.parse(cachedPost))
+    }
+    const response  =  await axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+    client.set(`post-${id}` , JSON.stringify(response.data))
+    return res.json(response.data)
 })
 
 client.on('ready', () => {
