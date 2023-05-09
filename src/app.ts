5a728b4d4ac5d3e3b938a1f0f6bcc55ae8c6f353
sijibomi <gbemilesanmi@gmail.com>
2023-05-10 00:11:20 +0100

    chore: bug fixes for redis connect

--- src/app.ts
+++ src/app.ts
@@ -2,26 +2,41 @@ import Express from "express"
 
 const app = Express()
 const PORT  = process.env.PORT || 5173
-import redis , {RedisClientType} from "redis"
+import * as redis from "redis"
 
 
-const redisUrl = "redis://127.0.0.1:6379"
-const client:RedisClientType = redis.createClient({url:redisUrl})
-
+const redisUrl:any = "redis://127.0.0.1:6379"
+const client:redis.RedisClientType = redis.createClient({url:redisUrl});
 
 app.use(Express.json())
 
-app.post("/" , (req , _res) => {
-    const {key , value} = req.body
-    client.set(key , value)
+app.get("/" , (_req , res) => {
+    res.json({message:"PONG"})
 })
 
 
+app.post("/" , async (req , res) => {
+    const {key , value} = req.body
+    console.log(key, value)
+    let response 
+    try {
+        response = await client.set(key , value)
+        console.log(response)
+        return res.json(response)
+    } catch (error) {
+        console.error(error)
+        return res.status(500).json({error})
+    }
+})
 
+client.on('ready', () => {
+  console.log('Redis client is ready');
+});
 
-
-app.listen(PORT , ()=>{
-    console.log("Server is listening...")
+app.listen(PORT ,async ()=>{
+    console.clear()
+    await client.connect();
+    console.log("Server is listening and redis is connected...")
 })
 
 
